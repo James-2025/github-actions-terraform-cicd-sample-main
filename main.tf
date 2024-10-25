611eeba40f85aa11f73a4ab88fc049cb1761b17f
James-2025 <4al20cs052@gmail.com>
2024-10-25 12:52:23 +0530

    c012

--- main.tf
+++ main.tf
@@ -10,38 +10,6 @@ provider "aws" {
   region = var.aws_region
 }
 
-data "aws_ami" "amazon_linux" {
-  most_recent = true
-  owners      = ["amazon"]
-
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
-  }
-}
-
-resource "aws_instance" "example" {
-  ami           = data.aws_ami.amazon_linux.id
-  instance_type = var.instance_type
-  key_name      = var.key_name
-
-  tags = {
-    Name = var.name
-  }
-
-  user_data = <<-EOF
-              #!/bin/bash
-              sudo yum -y update
-              sudo yum -y install httpd
-              sudo systemctl start httpd
-              sudo systemctl enable httpd
-              EOF
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
 # Azure Provider
 provider "azurerm" {
   features {}
@@ -52,7 +20,10 @@ provider "azurerm" {
   tenant_id       = var.azure_tenant_id
 }
 
-# AWS Instance
+
+
+
+
 data "aws_ami" "amazon_linux" {
   most_recent = true
   owners      = ["amazon"]
@@ -84,6 +55,9 @@ resource "aws_instance" "example" {
     create_before_destroy = true
   }
 }
+
+
+
 
 # Azure Resources
 resource "azurerm_resource_group" "example_rg" {
