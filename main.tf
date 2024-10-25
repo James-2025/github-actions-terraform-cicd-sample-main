2061798ddbc5f527b60b33d32bcf1ef9e664eecd
James-2025 <4al20cs052@gmail.com>
2024-10-25 12:31:54 +0530

    c006

--- main.tf
+++ main.tf
@@ -1,15 +1,27 @@
 terraform {
   backend "s3" {
-    bucket         = "bj001"
+    bucket         = var.s3_bucket_name
     key            = "terraform.tfstate"
-    region         = "eu-north-1"
+    region         = var.aws_region
   }
 }
 
+# AWS Provider
 provider "aws" {
   region = var.aws_region
 }
 
+# Azure Provider
+provider "azurerm" {
+  features {}
+
+  client_id       = var.azure_client_id
+  client_secret   = var.azure_client_secret
+  subscription_id = var.azure_subscription_id
+  tenant_id       = var.azure_tenant_id
+}
+
+# AWS Instance
 data "aws_ami" "amazon_linux" {
   most_recent = true
   owners      = ["amazon"]
@@ -40,4 +52,59 @@ resource "aws_instance" "example" {
   lifecycle {
     create_before_destroy = true
   }
+}
+
+# Azure Resources
+resource "azurerm_resource_group" "example_rg" {
+  name     = "${var.azure_instance_name}-rg"
+  location = var.azure_region
+}
+
+resource "azurerm_virtual_network" "example_vnet" {
+  name                = "${var.azure_instance_name}-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.example_rg.location
+  resource_group_name = azurerm_resource_group.example_rg.name
+}
+
+resource "azurerm_subnet" "example_subnet" {
+  name                 = "${var.azure_instance_name}-subnet"
+  resource_group_name  = azurerm_resource_group.example_rg.name
+  virtual_network_name = azurerm_virtual_network.example_vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_network_interface" "example_nic" {
+  name                = "${var.azure_instance_name}-nic"
+  location            = azurerm_resource_group.example_rg.location
+  resource_group_name = azurerm_resource_group.example_rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.example_subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "example_vm" {
+  name                  = var.azure_instance_name
+  location              = azurerm_resource_group.example_rg.location
+  resource_group_name   = azurerm_resource_group.example_rg.name
+  network_interface_ids = [azurerm_network_interface.example_nic.id]
+  size                  = "Standard_B1s"
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+
+  admin_username = "azureuser"
+  admin_password = var.azure_admin_password
 }
