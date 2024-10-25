88697d5851f47680cf43141b2e1ac346d051c90d
James-2025 <4al20cs052@gmail.com>
2024-10-25 12:54:19 +0530

    c013

--- variables.tf
+++ variables.tf
@@ -23,3 +23,37 @@ variable "name" {
   default = "instancetrial"
 }
 
+
+
+
+# Azure-specific variables
+variable "azure_client_id" {
+  description = "Azure Service Principal Client ID"
+}
+
+variable "azure_client_secret" {
+  description = "Azure Service Principal Client Secret"
+}
+
+variable "azure_subscription_id" {
+  description = "Azure Subscription ID"
+}
+
+variable "azure_tenant_id" {
+  description = "Azure Tenant ID"
+}
+
+variable "azure_region" {
+  description = "Azure region to deploy resources"
+  default     = "East US"
+}
+
+variable "azure_instance_name" {
+  description = "Name of the Azure instance"
+  default     = "azure-instance"
+}
+
+variable "azure_admin_password" {
+  description = "Admin password for the Azure VM"
+  sensitive   = true
+}
