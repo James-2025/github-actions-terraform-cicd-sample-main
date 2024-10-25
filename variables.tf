f07c7ffe84faa13b17604dd8e5329c53ccf3192d
James-2025 <4al20cs052@gmail.com>
2024-10-25 12:46:44 +0530

    c010

--- variables.tf
+++ variables.tf
@@ -10,47 +10,16 @@ variable "instance_type" {
 
 variable "key_name" {
   description = "Name of the existing EC2 key pair"
-  default     = "2024"
+  default = "2024"
 }
 
 variable "s3_bucket_name" {
   description = "Name of the S3 bucket for remote state"
-  default     = "bj001"
+  default = "tfdemo16112023"
 }
 
 variable "name" {
-  description = "Name of the EC2 instance"
-  default     = "prod-instance"
+  description = "Name of EC2 Intance"
+  default = "instancetrial"
 }
 
-# Azure-specific variables
-variable "azure_client_id" {
-  description = "Azure Service Principal Client ID"
-}
-
-variable "azure_client_secret" {
-  description = "Azure Service Principal Client Secret"
-}
-
-variable "azure_subscription_id" {
-  description = "Azure Subscription ID"
-}
-
-variable "azure_tenant_id" {
-  description = "Azure Tenant ID"
-}
-
-variable "azure_region" {
-  description = "Azure region to deploy resources"
-  default     = "East US"
-}
-
-variable "azure_instance_name" {
-  description = "Name of the Azure instance"
-  default     = "azure-instance"
-}
-
-variable "azure_admin_password" {
-  description = "Admin password for the Azure VM"
-  sensitive   = true
-}
